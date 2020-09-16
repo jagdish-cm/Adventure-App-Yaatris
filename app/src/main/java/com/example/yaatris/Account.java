3601db845ba5c7eb5f956b1320b573b85f73852b
jagdish-cm <jagdishcm41297@gmail.com>
2020-09-16 11:11:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/yaatris/Account.java
@@ -0,0 +1,38 @@
+package com.example.yaatris;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Account#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Account extends Fragment {
+
+    public Account() {
+        // Required empty public constructor
+    }
+
+    public static Account newInstance() {
+        Account fragment = new Account();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+}
