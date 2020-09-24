048c925dc6b370e9f8d977663b49daafeb35318e
jagdish-cm <jagdishcm41297@gmail.com>
2020-09-24 11:18:06 +0530

    map added

--- app/src/main/java/com/example/yaatris/Account.java
+++ app/src/main/java/com/example/yaatris/Account.java
@@ -1,19 +1,38 @@
 package com.example.yaatris;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Account#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Account extends Fragment {
+
+public class Account extends Fragment implements View.OnClickListener {
+
+    private FirebaseAuth firebaseAuth;
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonSignup;
+    private ProgressDialog progressDialog;
 
     public Account() {
         // Required empty public constructor
@@ -24,6 +43,7 @@ public class Account extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +53,68 @@ public class Account extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        //initializing views
+        editTextEmail = (EditText) v.findViewById(R.id.editTextEmailAddress);
+        editTextPassword = (EditText) v.findViewById(R.id.editTextPassword);
+
+        buttonSignup = (Button) v.findViewById(R.id.buttonSignup);
+
+        progressDialog = new ProgressDialog(getActivity());
+
+        //attaching listener to button
+        buttonSignup.setOnClickListener((View.OnClickListener) this);
+        return v;
     }
+
+    private void registerUser(){
+
+        //getting email and password from edit texts
+        final String email = editTextEmail.getText().toString().trim();
+        final String password  = editTextPassword.getText().toString().trim();
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(getActivity(),"Please enter email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(getActivity(),"Please enter password",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //if the email and password are not empty
+        //displaying a progress dialog
+
+        progressDialog.setMessage("Registering Please Wait...");
+        progressDialog.show();
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Activity) getContext(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+                            //display some message here
+                            Toast.makeText(getActivity(),"Successfully registered",Toast.LENGTH_LONG).show();
+                        }else{
+                            //display some message here
+                            Toast.makeText(getActivity(),"Registration Error",Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        //calling register method on click
+        registerUser();
+    }
+
 }
