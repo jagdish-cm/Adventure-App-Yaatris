12dcdc2f60d7857b42ddb59708fe79d8960f13e7
jagdish-cm <jagdishcm41297@gmail.com>
2020-10-01 16:58:21 +0530

    map added

--- app/src/main/java/com/example/yaatris/Account.java
+++ app/src/main/java/com/example/yaatris/Account.java
@@ -2,6 +2,7 @@ package com.example.yaatris;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,10 +32,15 @@ public class Account extends Fragment implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
 
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private Button buttonSignup;
-    private ProgressDialog progressDialog;
+    private EditText Email;
+    private EditText Password;
+    private Button LogInButton;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+    FirebaseUser mUser;
+    String email, password;
+    ProgressDialog dialog;
+    public static final String userEmail="";
 
     public Account() {
         // Required empty public constructor
@@ -56,65 +64,90 @@ public class Account extends Fragment implements View.OnClickListener {
         View v = inflater.inflate(R.layout.fragment_account, container, false);
         firebaseAuth = FirebaseAuth.getInstance();
         //initializing views
-        editTextEmail = (EditText) v.findViewById(R.id.editTextEmailAddress);
-        editTextPassword = (EditText) v.findViewById(R.id.editTextPassword);
+        Email = (EditText) v.findViewById(R.id.loginEmail);
+        Password = (EditText) v.findViewById(R.id.loginPassword);
+        LogInButton= (Button) v.findViewById(R.id.buttonLogin);
+        dialog = new ProgressDialog(getActivity());
 
-        buttonSignup = (Button) v.findViewById(R.id.buttonSignup);
-
-        progressDialog = new ProgressDialog(getActivity());
+        mAuth = FirebaseAuth.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
 
         //attaching listener to button
-        buttonSignup.setOnClickListener((View.OnClickListener) this);
+//        LogInButton.setOnClickListener((View.OnClickListener) this);
+
+        LogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Calling EditText is empty or no method.
+                userSign();
+            }
+        });
+
+
+        ImageButton userReg = (ImageButton) v.findViewById(R.id.userImgRegBtn);
+        userReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getActivity(), RegisterUser.class);
+                startActivity(in);
+            }
+        });
+
+        ImageButton companyReg = (ImageButton) v.findViewById(R.id.compRegImgBtn);
+        companyReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in2 = new Intent(getActivity(), RegisterCompany.class);
+                startActivity(in2);
+            }
+        });
+
+        ImageButton volReg = (ImageButton) v.findViewById(R.id.volRegImgBtn);
+        volReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in3 = new Intent(getActivity(), RegisterVolunteer.class);
+                startActivity(in3);
+            }
+        });
+
+
         return v;
     }
 
-    private void registerUser(){
-
-        //getting email and password from edit texts
-        final String email = editTextEmail.getText().toString().trim();
-        final String password  = editTextPassword.getText().toString().trim();
-
-        //checking if email and passwords are empty
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(getActivity(),"Please enter email",Toast.LENGTH_LONG).show();
+    private void userSign() {
+        email = Email.getText().toString().trim();
+        password = Password.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getActivity(), "Enter the correct Email", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getActivity(), "Enter the correct password", Toast.LENGTH_SHORT).show();
             return;
         }
-
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(getActivity(),"Please enter password",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        //if the email and password are not empty
-        //displaying a progress dialog
-
-        progressDialog.setMessage("Registering Please Wait...");
-        progressDialog.show();
-
-        //creating a new user
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener((Activity) getContext(), new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        //checking if success
-                        if(task.isSuccessful()){
-                            //display some message here
-                            Toast.makeText(getActivity(),"Successfully registered",Toast.LENGTH_LONG).show();
-                        }else{
-                            //display some message here
-                            Toast.makeText(getActivity(),"Registration Error",Toast.LENGTH_LONG).show();
-                        }
-                        progressDialog.dismiss();
-                    }
-                });
-
+        dialog.setMessage("Logging in please wait...");
+        dialog.setIndeterminate(true);
+        dialog.show();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    dialog.dismiss();
+                    Toast.makeText(getActivity(), "Login failed", Toast.LENGTH_SHORT).show();
+                } else {
+                    dialog.dismiss();
+                    Toast.makeText(getActivity(), "Logged in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
+
+
 
     @Override
     public void onClick(View view) {
         //calling register method on click
-        registerUser();
     }
 
 }
